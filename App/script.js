e72087abed16d4fef54d991631998a8af6c189c4
sachin <sachinkannojiya990@gmail.com>
2023-07-04 10:55:49 +0530

    disable and finished sorting

--- App/script.js
+++ App/script.js
@@ -17,3 +17,21 @@ async function setup() {
 function reset() {
 	location.reload();
 }
+
+function Disable_The_Input() {
+	let x = document.getElementsByTagName("input");
+	for (let i = 0; i < x.length; i++)
+		x[i].disabled = true;
+	return parseInt(document.getElementById("delay").value);
+}
+
+
+function Finished_Sorting() {
+	let x = document.getElementsByClassName("bar");
+	for (let i = 0; i < x.length; i++)
+		x[i].style.backgroundColor = finished;
+	x = document.getElementsByTagName("input");
+	for (let i = 0; i < x.length; i++)
+		x[i].disabled = false;
+
+}
